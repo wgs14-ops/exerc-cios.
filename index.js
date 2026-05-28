057f2a7e1b419b4214582ae8268972a134f5160b
Williane Gonçalves <wgs14@discente.ifpe.edu.br>
2026-05-28 12:00:19 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,16 +11,30 @@ app.get('/sobre', (req, res) => {
   res.send('Página Sobre');
 });
 
+//exercício 3
+app.get('/contato', (req, res) => {
+  res.json({
+    email: 'contato@gmail.com',
+    telefone: '(81) 99999-9999'
+  });
+});
+
+//exercício 4
+app.get('/erro', (req, res) => {
+  res.status(404).send('Página não encontrada');
+});
+
+//exercício 5
+app.get('/inicio/', (req, res) => {
+  res.redirect('/')
+});
+
+//exercício 6
+app.get('/usuarios/:id', (req, res) => {
+  res.send('Página Sobre');
+});
+
 app.listen(3000, () => {
   console.log('servidor em execução');
-});
-
-//exercício 2
-app.get('/contato', (req, res) => {
-  res.send('email: contato@gmail.com  \ntelefone: (81) 99999-9999');
-});
-
-app.get('/erro', (req, res) => {
-  res.send('status HTTP 404 mensagem Página não encontrada');
 });
 
